d7c4f555bbd31b2291b931b3905fd270f9953331
jaramutiar <rafijaramutiar02@gmail.com>
2024-05-15 15:56:46 +0700

    tugasku

---
+++ database/migrations/2024_05_15_084326_create_kritiks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kritiks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('film_id');
+            $table->text('content');
+            $table->integer('poiint');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kritiks');
+    }
+};
